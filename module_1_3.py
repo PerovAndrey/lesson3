15277760514f0f62eba70593101b355281101e1c
PerovAndrey <nikita.vasiliev1982@mail.ru>
2024-08-11 02:23:19 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,18 @@
+#lesson
+name = "Urban"
+print(name, type(name))
+name = 5
+print(name, type(name))
+name = 5.5
+print(name, type(name))
+name = [1, 2]
+print(name, type(name))
+#practical task
+name = "Andrei"
+print(name)
+age = 32
+print(age)
+age = age + 10
+print(age)
+is_student = True
+print(is_student)
